8b852677d35f2e52be34f1554f47eaa80dcf6733
abhisheksharma911 <abhiak012@gmail.com>
2024-06-03 16:03:36 +0530

    new

--- hospital.js
+++ hospital.js
@@ -1,7 +1,20 @@
 let slide = document.querySelectorAll(".patientReview");
-let count = 0;
+let card = document.querySelectorAll(".card");
 
 slide.forEach(function(slides,index){
     slides.style.left='${index * 100}%
 })
 
+function myFun(){
+    slide.forEach(function(cur$Val){
+        curVal.style.transform='translatteX(-${count * 100}%)'
+    })
+}
+
+setInterval(function(){
+    count++;
+    if (count == slide.length){
+        count =0;
+    }
+    myFun()
+},2000)
