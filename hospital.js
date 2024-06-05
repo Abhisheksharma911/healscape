0b611c9ab15b718e557dc01ccce7dab28fa83487
abhisheksharma911 <abhiak012@gmail.com>
2024-06-05 09:34:51 +0530

    9:30 am

--- hospital.js
+++ hospital.js
@@ -1,8 +1,11 @@
 let slide = document.querySelectorAll(".patientReview");
 let card = document.querySelectorAll(".card");
+let closeBtn = document.getElementById("closeBtn");
+let connectBtn = document.getElementById("connectBtn");
 
 slide.forEach(function(slides,index){
-    slides.style.left='${index * 100}%
+    slides.style.left='${index * 100}%'
+    
 })
 
 function myFun(){
@@ -18,3 +21,33 @@ setInterval(function(){
     }
     myFun()
 },2000)
+
+card.forEach(function(cards){
+    cards.addEventListener("click",function(){
+        console.log(card);
+
+        document.querySelector(".detail").style.display="block"
+        document.querySelector(".content").innerHTML='
+        <img src=${card.firstElementChild.src} alt="">
+        <div class="contentText">
+                <h1>Alexa Zoan</h1>
+                <p></p>
+        </div>  
+        '
+        closeBtn.addEventListener("click",function(){
+            document.querySelector(".detail").style.display="none"
+        })
+    })
+})
+
+connectBtn.addEventListener("click",function(){
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+
+    if(MediaList.value == "" && pass.value ==""){
+        alert("Enter Details")
+    }else{
+        alert("You Logged In")
+    }
+    
+})
